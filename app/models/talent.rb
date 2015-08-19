d1fb94842bd697e91ed439990f6cfdd57053b097
Prelang Builder <builder@prelang.com>
2015-08-19 17:42:21 +0000

    Defined validation for attribute 'talentphone' on model 'Talent'

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -2,4 +2,5 @@ class Talent < ActiveRecord::Base
   belongs_to :job
   has_and_belongs_to_many :jobs
   validates_formatting_of :talentemail, using: :email
+  validates_formatting_of :talentphone, using: :us_phone
 end
