2cb9ac63c1af0cc388c38b99dd0cd5a0bd5c2ab2
Prelang Builder <builder@prelang.com>
2015-08-19 17:42:21 +0000

    Defined validation for attribute 'talentzip' on model 'Talent'

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -3,4 +3,5 @@ class Talent < ActiveRecord::Base
   has_and_belongs_to_many :jobs
   validates_formatting_of :talentemail, using: :email
   validates_formatting_of :talentphone, using: :us_phone
+  validates_formatting_of :talentzip, using: :us_zip
 end
